5f1892ada00783f96444babde7956a2be0d8679a
Kathy Chan <kathykemopaw@gmail.com>
2020-10-14 11:13:29 -0700

    Changed the login session to take in email than username

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -9,10 +9,10 @@ class Api::SessionsController < ApplicationController
 
     def create
         @user = User.find_by_credentials(
-            params[:user][:username],
+            params[:user][:email],
             params[:user][:password]
         )
-
+            
         if @user
             login!(@user)
             render "api/session/show" #check pokedex for how to tell rails to render that jbuild file!
