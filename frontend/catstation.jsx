6d45e953781180bb97dc2e6f97efe3649b89909c
Kathy Chan <kathykemopaw@gmail.com>
2020-10-14 11:56:23 -0700

    Connected to Root Component from the React Entry file

--- frontend/catstation.jsx
+++ frontend/catstation.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import {signup, login, logout} from "./actions/session_actions";
 import configureStore from './store/store'
 
+import Root from './components/root';
+
 //eventlistener, waits for the event
 //once that event happens, it invokes the cb function to do its job
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     const store = configureStore();
-    ReactDOM.render(<h1>Hello World</h1>, root);
+    ReactDOM.render(<Root store={store}/>, root);
     window.login = login;
     window.signup = signup;
     window.logout = logout;
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,7 +1,11 @@
-import session from './session_reducer';
 import { combineReducers } from "redux";
 
+import entities from './entities_reducer';
+import session from './session_reducer';
+
+
 const rootReducer = combineReducers({
+    entities,
     session
 })
 
