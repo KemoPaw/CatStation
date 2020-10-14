d8f71f95a2218a37d766e61319a6ecfbbb7ef195
Kathy Chan <kathykemopaw@gmail.com>
2020-10-14 14:41:14 -0700

    Adding in more files, still need to work on bug fixes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import {Provider} from 'react-redux';
 
 import {Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
+import GreetingContainer from './greeting/greeting_container';
+import SignUpFormContainer from './session_form/signup_form_container';
+import LogInFormContainer from './session_form/login_form_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
     <div>
+        <header>
+            <GreetingContainer />
+        </header>
         <h1>Hello! from the App.jsx!</h1>
+        <AuthRoute exact path="/login" component={LogInFormContainer} />
+        <AuthRoute exact path="/signup" component={SignUpFormContainer} />
     </div>
 );
 
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 
 import entities from './entities_reducer';
 import session from './session_reducer';
+import errors from './session_errors_reducer'
 
 
 const rootReducer = combineReducers({
     entities,
-    session
+    session,
+    errors
 })
 
 export default rootReducer;
