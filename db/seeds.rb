ba7ccad4892034605ebd94ab91fb663253146ae8
Kathy Chan <kathykemopaw@gmail.com>
2020-10-20 18:09:55 -0700

    Seed the images to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,5 +27,18 @@ photo7 = Photo.create({title: "photo7", description: "photo7 des", uploader_id: 
 photo8 = Photo.create({title: "photo8", description: "photo8 des", uploader_id: seeduser2.id})
 photo9 = Photo.create({title: "photo9", description: "photo9 des", uploader_id: seeduser2.id})
 photo10 = Photo.create({title: "photo10", description: "photo10 des", uploader_id: seeduser2.id})
+photo11 = Photo.create({title: "photo11", description: "photo11 des", uploader_id: seeduser3.id})
+photo12 = Photo.create({title: "photo12", description: "photo12 des", uploader_id: seeduser3.id})
+photo13 = Photo.create({title: "photo13", description: "photo13 des", uploader_id: seeduser3.id})
+photo14 = Photo.create({title: "photo14", description: "photo14 des", uploader_id: seeduser3.id})
+photo15 = Photo.create({title: "photo15", description: "photo15 des", uploader_id: seeduser3.id})
+photo16 = Photo.create({title: "photo16", description: "photo16 des", uploader_id: seeduser4.id})
+photo17 = Photo.create({title: "photo17", description: "photo17 des", uploader_id: seeduser4.id})
+photo18 = Photo.create({title: "photo18", description: "photo18 des", uploader_id: seeduser4.id})
+photo19 = Photo.create({title: "photo19", description: "photo19 des", uploader_id: seeduser4.id})
+photo20 = Photo.create({title: "photo20", description: "photo20 des", uploader_id: seeduser4.id})
+
+
+photo1.image.attach(io: open('https://catstation-seeds.s3-us-west-1.amazonaws.com/cat1-1.jpg'), filename: 'cat1-1.jpg')
 
 
