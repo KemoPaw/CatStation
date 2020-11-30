98347f70575c12a9449ec145795e76e1874c8df5
Kathy Chan <kathykemopaw@gmail.com>
2020-11-29 22:02:01 -0800

    Able to render on photoshow uplaoders username

--- frontend/components/photo/photo_show.jsx
+++ frontend/components/photo/photo_show.jsx
@@ -4,6 +4,10 @@ class PhotoShow extends React.Component{
     constructor(props){
         super(props);
         this.removePhoto = this.removePhoto.bind(this);
+        this.userIdToUsername = this.userIdToUsername.bind(this);
+    }
+
+    componentDidMount(){
     }
 
     removePhoto(){
@@ -22,6 +26,30 @@ class PhotoShow extends React.Component{
        
     }
 
+    userIdToUsername(userId){
+        console.log(this.props.users);
+        // console.log(Object.values(this.props.users));
+        // console.log(typeof Object.values(this.props.users));
+        let username = "";
+
+        Object.values(this.props.users).map(ele => {
+            if(ele.id === userId){
+                // console.log(ele.id);
+                username = this.props.users[ele.id].username;
+            }
+        })
+
+        return username;
+
+        // console.log(typeof this.props.users);
+        // console.log("Hello");
+
+        // console.log(Object.keys(this.props.users));
+
+       
+        
+    }
+
 
     render(){
         if (!this.props.photo) return null;
@@ -35,9 +63,10 @@ class PhotoShow extends React.Component{
 
                 <div className="photo-show-content">
                     <h1 className="photo-show-title">{this.props.photo.title}</h1>
-                    <h1 color="white">{this.props.photo.uploader_id}</h1>
+                    <h1 className="photo-show-title">{this.userIdToUsername(this.props.photo.uploader_id)}</h1>
+                    {/* <h1 className="photo-show-title">{this.props.users}</h1> */}
+
                     <h3 className="photo-show-text">{this.props.photo.description}</h3>
-                    <h3>{this.props.currentUser}</h3>
 
                     <div>
                         {/* {this.props.photo.uploader_id === this.state.user.id ? <button className="photo-show-remove" type="button" onClick={() => this.removePhoto()}>Remove Photo</button> : <div></div>} */}
