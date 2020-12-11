350d9f5f4a4f093b24b4bc0bb50dd31f058f8754
Kathy Chan <kathykemopaw@gmail.com>
2020-12-11 14:59:38 -0800

    Photo SHow index styling

--- frontend/components/photo/photo_index_item.jsx
+++ frontend/components/photo/photo_index_item.jsx
@@ -6,10 +6,13 @@ const PhotoIndexItem = props => {
     return (
 
         <li className="photo-index-li">
+                <Link to={`/photos/${props.photo.id}`}>
+                    <img width="100%" className="photo-index-img" src={props.photo.photoUrl} />
+                </Link>
+            <div className="photo-index-item-div">
+                
+            </div>
             
-            <Link className="sample" to={`/photos/${props.photo.id}`}>
-                <img width="100%" className="photo-index-img" src={props.photo.photoUrl} />
-            </Link>
 
         </li>
     )
