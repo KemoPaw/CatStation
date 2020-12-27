03975a410da3388ad10e9742a14b37571dcde4ed
Kathy Chan <kathykemopaw@gmail.com>
2020-12-26 21:26:18 -0800

    Able to search by photo title even if the search is only part of a query

--- frontend/components/search_bar/search_index.jsx
+++ frontend/components/search_bar/search_index.jsx
@@ -7,30 +7,28 @@ class SearchIndex extends React.Component {
     componentDidMount(){
         this.props.fetchPhotos();
         this.props.fetchUsers();
-
-       
-
-        // console.log(photoArrItem);
     }
 
     render() {
         const { photos } = this.props;
         let arr = this.props.location.pathname.split("/");
         let photoArrItem = arr[arr.length -1];
-
+        // console.log(photoArrItem);
         return(
             <div className="photo-index-div">
-                <p color="white">SEARCH INDEX CONTAINER</p>
+                {/* <p color="white">SEARCH INDEX CONTAINER</p> */}
+                <div>
+                    <p>Search Results for: {photoArrItem}</p>
+                </div>
                 <ul className="photo-index-ul">
                     {
                         photos.map(photo=> {
-                              if(photoArrItem === photo.title){
+                              if(photo.title.toLowerCase().includes(photoArrItem.toLowerCase())){
                                 return (<SearchItem
                                     key={photo.id}
                                     id={photo.id}
 
                                     photo={photo}
-                                    // photos={photos}
                                  />)
                                 }
                             else{
